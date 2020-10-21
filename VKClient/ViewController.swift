b2011156541924fe029720408f5c3f3d76a8ea0b
Nikita Plakhin <nplkhn1@yandex.ru>
2020-10-21 12:55:04 +0300

    Initial Commit

---
+++ VKClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKClient
+//
+//  Created by Никита Плахин on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
